84caaa0a74a1f887fab5818669d83a901dec2b70
cheachum <56030609+cheachum@users.noreply.github.com>
2019-10-23 21:23:50 -0400

    PRS-ConsoleApp

---
+++ Vendor.java
@@ -0,0 +1,85 @@
+package business;
+
+public class Vendor {
+	private int id; 
+	private int code;
+	private String name;
+	private String address;
+	private String city;
+	private String state; 
+	private String phoneNumber;
+	private String email;
+	
+	public Vendor() {
+		super();
+	}
+	public Vendor(int id, int code, String name, String address, String city, String state, String phoneNumber,
+			String email) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Vendor [id=" + id + ", code=" + code + ", name=" + name + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+	
+	
+	
+
+}
